35357c8ce2dd01121113dc718d438559a23d9b4f
eun-choi <54615424+eun-choi@users.noreply.github.com>
2022-06-16 15:27:26 +0900

    update 4.0

--- src/android/ConnectPlugin.java
+++ src/android/ConnectPlugin.java
@@ -251,11 +251,11 @@ public class ConnectPlugin extends CordovaPlugin {
         AppEventsLogger.activateApp(cordova.getActivity().getApplication());
     }
 
-    @Override
+    /*@Override
     public void onPause(boolean multitasking) {
         super.onPause(multitasking);
         AppEventsLogger.deactivateApp(cordova.getActivity().getApplication());
-    }
+    }*/
 
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent intent) {
@@ -272,6 +272,14 @@ public class ConnectPlugin extends CordovaPlugin {
 
         } else if (action.equals("setApplicationId")) {
             executeSetApplicationId(args, callbackContext);
+            return true;
+
+        } else if (action.equals("getClientToken")) {
+            callbackContext.success(FacebookSdk.getClientToken());
+            return true;
+
+        } else if (action.equals("setClientToken")) {
+            executeSetClientToken(args, callbackContext);
             return true;
 
         } else if (action.equals("getApplicationName")) {
@@ -392,6 +400,22 @@ public class ConnectPlugin extends CordovaPlugin {
             callbackContext.success();
         } catch (JSONException e) {
             callbackContext.error("Error setting application ID");
+        }
+    }
+
+    private void executeSetClientToken(JSONArray args, CallbackContext callbackContext) {
+        if (args.length() == 0) {
+            // Not enough parameters
+            callbackContext.error("Invalid arguments");
+            return;
+        }
+
+        try {
+            String clientToken = args.getString(0);
+            FacebookSdk.setClientToken(clientToken);
+            callbackContext.success();
+        } catch (JSONException e) {
+            callbackContext.error("Error setting client token");
         }
     }
 
